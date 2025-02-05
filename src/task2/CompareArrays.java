0ee025383b63ba4497af888edc6d5b2d92d4b61e
Kirek <kirek_priest@mail.ru>
2025-02-06 00:22:54 +0500

    small changes

--- src/task2/CompareArrays.java
+++ src/task2/CompareArrays.java
@@ -3,10 +3,10 @@ package task2;
 import java.util.Objects;
 
 public class CompareArrays {
-    public <T> boolean compareArrays(T[] arr1, T[] arr2){
+    public <T, U> boolean compareArrays(T[] arr1, U[] arr2){
         if (arr1.length == arr2.length){
             for (int i = 0; i < arr1.length; i++) {
-                if (arr1[i].getClass().getName() != arr2[i].getClass().getName()){
+                if (!arr1[i].getClass().getName().equals(arr2[i].getClass().getName())){
                     return false;
                 }
             }
